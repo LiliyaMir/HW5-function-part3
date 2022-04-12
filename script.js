339c05678e15e0eaa1845fd58040805d2751e874
Lilia Mironova <lilyamironova67@gmail.com>
2022-04-12 19:18:33 +0300

    2attemp

--- script.js
+++ script.js
@@ -1,35 +1,24 @@
 console.log('_____________#4b')
 function range(a, b) {
   if (a<b) {
-    if (a == b) {
-      return; 
-    }
-    console.log(a);
     range(a+1, b);
   }
   else if (a>b) {
-    if (a == b) {
-      return;
-    }
-    console.log(a);
     range(a-1, b);
   }
-  else {
-    console.log(a);
-  }
+  console.log(a);
 }
 range(2, 8);
 console.log(' ');
 range(8, 2);
 
 // console.log('_____________#5')
-var time = setInterval(function() {
-  timer();
-}, 1000);
+setInterval(timer, 1000);
 
+var clockB = document.getElementById("clock");
 function timer() {
   var clock = new Date();
-  document.getElementById("clock").innerHTML = clock.toLocaleTimeString();
+  clockB.innerHTML = clock.toLocaleTimeString();
 }
 
 
